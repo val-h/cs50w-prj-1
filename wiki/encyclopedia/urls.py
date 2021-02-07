2f2c164b1fea56c5eedb0acc8561a744db224b39
Valentin Mirchev <valentin97mirchev@gmail.com>
2021-02-07 02:15:23 +0000

    Entry page and Index Page - Finished

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.wiki_page, name="wiki_page"),
+    path("wiki/<str:title>", views.search_page, name="search_page"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -4,7 +4,24 @@ from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    context = {
+        'entries': util.list_entries()
+    }
+    return render(request, "encyclopedia/index.html", context=context)
+
+def wiki_page(request, title):
+    context = {
+        'title': title,
+        # To make a custom md to html parser for later
+        'content': util.get_entry(title),
+    }
+    if context['content'] == None:
+        return render(request, 'encyclopedia/404.html', context=context)
+    else:
+        return render(request, 'encyclopedia/wiki_page.html', context=context)
+
+# Tried to make a search query while using wiki_page but it needs it parameter
+# to work with the previous feature, directly typing it in the url.
+def search_page(request):
+    pass
 
